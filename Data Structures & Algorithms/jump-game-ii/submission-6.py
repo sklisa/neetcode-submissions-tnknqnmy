49e24878d17312120501f12eb3aa3250ec727238
Ke Sun <44296482+sklisa@users.noreply.github.com>
2026-03-30 12:11:39 -0700

    Add: jump-game-ii - submission-6

---
+++ Data Structures & Algorithms/jump-game-ii/submission-6.py
@@ -0,0 +1,15 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        # DP
+        # Time O(N^2), Space O(N)
+
+        dp = [2**31] * len(nums)
+        dp[-1] = 0 # last index
+
+        for i in range(len(nums)-2, -1, -1): # right to left    
+            # for each index, the step is, 1 + min of step of all reachable index
+            right = min(len(nums), i + nums[i] + 1)
+            for j in range(i, right):
+                dp[i] = min(dp[i], dp[j] + 1)
+
+        return dp[0]
