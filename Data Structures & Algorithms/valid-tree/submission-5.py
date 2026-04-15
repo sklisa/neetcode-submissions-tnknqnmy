c7b5dbb9e1109e3caef28c7db8f2e0433663b616
Ke Sun <44296482+sklisa@users.noreply.github.com>
2026-04-15 11:48:20 -0700

    Add: valid-tree - submission-5

---
+++ Data Structures & Algorithms/valid-tree/submission-5.py
@@ -0,0 +1,30 @@
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        # Time O(N), Space O(N)
+        # Cycle detection, DFS
+        if len(edges) > (n-1):
+            return False
+
+        neighbors = defaultdict(set)
+        for a, b in edges:
+            neighbors[a].add(b)
+            neighbors[b].add(a)
+        
+        res = True
+        visited = set([0])
+        def dfs(parent, curr):
+            nonlocal res
+            nonlocal visited
+            visited.add(curr)
+            for nb in neighbors[curr]:
+                if nb != parent:
+                    if nb in visited:
+                        res = False
+                        return
+                    dfs(curr, nb)
+
+        for i in neighbors[0]:
+            dfs(0, i)
+        
+        return res if len(visited) == n else False
+        
