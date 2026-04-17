a2248e56fc9925a13c27b6a87f690b883d792e80
Ke Sun <44296482+sklisa@users.noreply.github.com>
2026-04-16 18:40:32 -0700

    Add: word-ladder - submission-2

---
+++ Data Structures & Algorithms/word-ladder/submission-2.py
@@ -0,0 +1,32 @@
+class Solution:
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        def replaceChar(word, ind):
+            wlist = list(word)
+            wlist[ind] = '*'
+            return ''.join(wlist)
+        
+        # Time O(LN^2), Space O(NL)
+        adjlist = defaultdict(set)
+        for w in wordList:
+            for i in range(len(w)):
+                wstar = replaceChar(w, i)
+                adjlist[wstar].add(w)
+        
+        q = deque([beginWord])
+        res = 0
+        visited = set([beginWord])
+        while q:
+            currlen = len(q)
+            res += 1
+            for i in range(currlen):
+                curr = q.popleft()
+                if curr == endWord:
+                    return res
+                for j in range(len(curr)):
+                    currstar = replaceChar(curr, j)
+                    for nb in adjlist[currstar]:
+                        if nb not in visited:
+                            visited.add(nb)
+                            q.append(nb)
+        
+        return 0
